9101f3948e0058fdc3319120390d63a850190f05
bryan_pin96@hotmail.com <bryan_pin96@hotmail.com>
2022-06-29 20:24:24 -0500

    primer cambio

---
+++ freelancer_inicio/php/01-hola-mundo.php
@@ -0,0 +1,17 @@
+<?php include 'includes/header.php'; ?>
+
+<?php echo "hola Mundo"; ?>
+
+<br>
+
+<?php echo ("Hola mundo");
+
+print ("Hola mundo");
+print "Hola mundo";
+print_r("Hola Mundo");
+// var_dump: nos devuelte el tipo de dato que tiene el texto o variable
+var_dump("Hola Mundo");
+
+print("Bryan Steven aqui");
+
+include 'includes/footer.php'; ?>
